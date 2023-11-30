a8558c40cc66dd19dd1c1433d0146da3ed5f5911
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-12-01 00:11:51 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,8 @@
 if __name__ == "__main__":
     import sys
 
-    count = len(sys.argv) - 1
-    if count == 0:
+    count = len(sys.argv) - 1  # Subtract 1 to exclude the script name
+    if count == 0:  # check no of args and print accordingly
         print("{} arguments.".format('0'))
     elif count == 1:
         print("{} argument:".format(count))
