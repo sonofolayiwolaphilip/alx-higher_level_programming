aea99feac2cf0ef59288305478409f551814033a
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-12-06 01:11:48 +0100

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-# function that replaces an element of a list at
-# a specific position
+    # function that replaces an element of a list at
+    # a specific position
     if idx < 0 or idx >= len(my_list):
         return my_list
     my_list[idx] = element
