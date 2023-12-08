09982f537e8b1f49b2dda42023086d23b74b9454
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-12-08 03:40:10 +0100

    update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def uniq_ad(my_list=[]):
-    return sum(set(my_list))
-   
-    # unique_set = set(my_list)
-    # add_result = sum(unique_set)
-    # return add_result
+    unique_set = set(my_list)
+    add_result = sum(unique_set)
+    return add_result
+
+    # list comprehension way
+    #  return sum(set(my_list))
