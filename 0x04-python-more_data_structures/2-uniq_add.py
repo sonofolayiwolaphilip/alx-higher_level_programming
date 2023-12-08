753a98d4c087b8d3d7aced3240d52ee38e77ac3e
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2023-12-08 03:38:11 +0100

    update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
-def uniq_add(my_list[]):
+
+def uniq_ad(my_list=[]):
     return sum(set(my_list))
+   
+    # unique_set = set(my_list)
+    # add_result = sum(unique_set)
+    # return add_result
