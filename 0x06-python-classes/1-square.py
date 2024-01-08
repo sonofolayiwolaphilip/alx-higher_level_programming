a63cfa4f6ece38e99e390db0782970d951bd7016
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2024-01-08 13:29:36 +0100

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+ a class Square that defines a square by: (based on 0-square.py)
+
+"""
+
+class Square:
+    """defining class of Square"""
+    def __init__(self, size):
+        self.__size = size
