f034dddbdd8b9e41388bfc3250e68196066b2af0
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2024-01-14 15:55:40 +0100

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -12,27 +12,33 @@ property def size(self): to retrieve property
 
 class Square:
     """
-
-        instantiation of size
-        if size os less than 0, raise a ValueError
-        exception with the message
-        size must be >=0
+    instantiating the variables self and size
+    Raising errors if conditions are not met
     """
-
     def __init__(self, size=0):
-        #   check for integers
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        #   check size
-        if size < 0:
-            raise ValueError("size must be >=0")
-
+        # Initialize private attribute
         self.__size = size
+    
+    @property # property to retrieve size
+    def size(self):
+        return self.__size
+
+    @size.setter    # Setter method for size
+    def size(self,value):
+        # Check if value is an integer
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        #   check if value is >= 0
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        # Update the private instance attribute
+        self.__size = value
 
     def area(self):
         """
         Calculates area of square
         Returns: area
         """
-
         return (self.__size ** 2)
