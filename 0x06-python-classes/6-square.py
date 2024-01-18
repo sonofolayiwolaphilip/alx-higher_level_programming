ecb49dcb1495f2c62472c865f27875ba4355d623
sonofolayiwolaphilip <sonofolayiwolaphilip@gmail.com>
2024-01-18 17:58:03 +0100

    update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,51 +19,51 @@ Method my_print prints the square using "#".
 
 class Square:
     """
-    Instantiate the variables self and size.
-    raising error if conditions are not met.
+    Instantiating the variables self and size.
+    Raising errors if conditions are not met.
     and print square using '#'.
     """
     def __init__(self, size=0, position=(0, 0)):
         # Initialize private attributes
         self.size = size
-        self.postion = position
+        self.position = position
 
-    @property  # propert to retrieve size
+    @property  # property to retrieve size (Getter method)
     def size(self):
         return self.__size
 
-    @size.setter
+    @size.setter  # Setter method for size
     def size(self, value):
-        """size setter method that sets the size of square.
+        """size setter  method that sets the size of square.
         Args:
             value (int): size of Square
         Raises:
-            TyprError: if value is not an integer.
-            ValueError: If value is less than 0.
+            TypeError: If `value` is not an integer.
+            ValueError: If `value` is less than 0.
 
         """
         # Check if value is an integer
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
-        # Check if value is an integer
+        # Check if value is >= 0
         if value < 0:
             raise ValueError("size must be >= 0")
 
-        # update the private instance attribute
+        # Update the private instance attribute
         self.__size = value
 
-    @property
+    @property  # property to retrieve position (Getter method)
     def position(self):
         return self.__position
 
-    @position.setter
+    @position.setter  # Setter method for position
     def position(self, value):
-        """position setter methon that sets position of Square.
+        """position setter method that sets position of Square.
         Args:
             value (tuple): tuple of two positive integer coordinates
         Raises:
-            TypeError: IF value is not a tuple of two positive integers
+            TypeError: If `value` is not a tuple of two positive integers
 
         """
         # verify that is a tuple of 2 positive integers
@@ -71,13 +71,13 @@ class Square:
                 len(value) != 2 or
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
-            raise TypeError("Position must be a tuple of 2 positive integers")
-        self.__position = value
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value  # Update the private instance attribute
 
     def area(self):
         """
-        Calculate area of square
-        returns: area
+        Calculates area of square
+        Returns: area
         """
         return (self.__size ** 2)
 
